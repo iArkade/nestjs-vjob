55c1e251fe863cfc5af3d7126a9477caf4f2a448
Daniel Velasco <desteban.velpi@gmail.com>
2024-11-20 15:29:23 -0500

    se agrgo el asiento entity en los modulos de datCentros y de transaccion

--- src/dat_centro/dat_centro.module.ts
+++ src/dat_centro/dat_centro.module.ts
@@ -3,9 +3,10 @@ import { DatCentroService } from './dat_centro.service';
 import { DatCentroController } from './dat_centro.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatCentro } from './entities/dat_centro.entity';
+import { Asiento } from 'src/asiento/entities/asiento.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([DatCentro])],
+  imports: [TypeOrmModule.forFeature([DatCentro, Asiento])],
   controllers: [DatCentroController],
   providers: [DatCentroService],
 })
--- src/transaccion-contable/transaccion-contable.module.ts
+++ src/transaccion-contable/transaccion-contable.module.ts
@@ -3,9 +3,10 @@ import { TransaccionContableService } from './transaccion-contable.service';
 import { TransaccionContableController } from './transaccion-contable.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransaccionContable } from './entities/transaccion-contable.entity';
+import { Asiento } from 'src/asiento/entities/asiento.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([TransaccionContable])],
+    imports: [TypeOrmModule.forFeature([TransaccionContable, Asiento])],
     controllers: [TransaccionContableController],
     providers: [TransaccionContableService],
 })
