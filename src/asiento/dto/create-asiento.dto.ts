0e9c1f910383598e6e352109c17ee9c4fcb00a1c
David Proaño <davisprofranco@hotmail.es>
2024-11-22 09:22:25 -0500

    feat: remove secuencial data

--- src/asiento/dto/create-asiento.dto.ts
+++ src/asiento/dto/create-asiento.dto.ts
@@ -1,98 +1,104 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsArray, IsDate, IsDateString, IsNumber, IsOptional, IsString, ValidateNested } from "class-validator";
-import { CreateAsientoItemDto } from "./create-asiento-item.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsDate,
+  IsDateString,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreateAsientoItemDto } from './create-asiento-item.dto';
 
 export class CreateAsientoDto {
+  @ApiProperty({
+    description: 'Fecha de emisión del asiento',
+    example: '2023-10-23',
+    format: 'date',
+  })
+  @IsDateString()
+  fecha_emision: string;
+  // @IsString()
+  // fecha_emision: string;
 
-     @ApiProperty({
-          description: 'Fecha de emisión del asiento',
-          example: '2023-10-23',
-          format: 'date'
-     })
-     @IsDateString()
-     fecha_emision: string;
-     // @IsString()
-     // fecha_emision: string;
+  @ApiProperty({
+    description: 'Número único del asiento',
+    example: 'AS00123',
+  })
+  @IsString()
+  nro_asiento: string;
 
-     @ApiProperty({
-          description: 'Número único del asiento',
-          example: 'AS00123',
-     })
-     @IsString()
-     nro_asiento: string;
+  @ApiProperty({
+    description: 'Comentario opcional',
+    example: 'Comentario sobre el asiento',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  comentario?: string;
 
-     @ApiProperty({
-          description: 'Comentario opcional',
-          example: 'Comentario sobre el asiento',
-          required: false,
-     })
-     @IsString()
-     @IsOptional()
-     comentario?: string;
+  @ApiProperty({
+    description: 'Tipo de transacción',
+    example: 'Compra',
+  })
+  @IsString()
+  codigo_transaccion: string;
 
-     @ApiProperty({
-          description: 'Tipo de transacción',
-          example: 'Compra',
-     })
-     @IsString()
-     codigo_transaccion: string;
+  @ApiProperty({
+    description: 'Estado del asiento',
+    example: 'Activo',
+  })
+  @IsString()
+  estado: string;
 
-     @ApiProperty({
-          description: 'Estado del asiento',
-          example: 'Activo',
-     })
-     @IsString()
-     estado: string;
+  @ApiProperty({
+    description: 'Número de referencia opcional',
+    example: 'REF12345',
+    required: false,
+  })
+  @IsString()
+  @IsOptional()
+  nro_referencia?: string;
 
-     @ApiProperty({
-          description: 'Número de referencia opcional',
-          example: 'REF12345',
-          required: false,
-     })
-     @IsString()
-     @IsOptional()
-     nro_referencia?: string;
+  // @ApiProperty({
+  //      description: 'Secuencial del asiento',
+  //      example: '00001',
+  // })
+  // @IsString()
+  // secuencial: string;
 
-     @ApiProperty({
-          description: 'Secuencial del asiento',
-          example: '00001',
-     })
-     @IsString()
-     secuencial: string;
+  @ApiProperty({
+    description: 'Código del centro',
+    example: 'CC001',
+  })
+  @IsString()
+  codigo_centro: string;
 
-     @ApiProperty({
-          description: 'Código del centro',
-          example: 'CC001',
-     })
-     @IsString()
-     codigo_centro: string;
+  @ApiProperty({
+    description: 'Código de la empresa',
+    example: 'EMP001',
+  })
+  @IsString()
+  @IsOptional()
+  codigo_empresa: string;
 
-     @ApiProperty({
-          description: 'Código de la empresa',
-          example: 'EMP001',
-     })
-     @IsString()
-     @IsOptional()
-     codigo_empresa: string;
+  @ApiProperty({
+    description: 'Total del debe',
+    example: 1000.0,
+  })
+  @IsNumber()
+  total_debe: number;
 
-     @ApiProperty({
-          description: 'Total del debe',
-          example: 1000.00,
-     })
-     @IsNumber()
-     total_debe: number;
+  @ApiProperty({
+    description: 'Total del haber',
+    example: 1000.0,
+  })
+  @IsNumber()
+  total_haber: number;
 
-     @ApiProperty({
-          description: 'Total del haber',
-          example: 1000.00,
-     })
-     @IsNumber()
-     total_haber: number;
-
-     @IsArray()
-     @ValidateNested({ each: true })
-     @Type(() => CreateAsientoItemDto)
-     lineItems: CreateAsientoItemDto[];
-
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateAsientoItemDto)
+  lineItems: CreateAsientoItemDto[];
 }
--- src/asiento/entities/asiento.entity.ts
+++ src/asiento/entities/asiento.entity.ts
@@ -1,47 +1,47 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { AsientoItem } from "./asiento-item.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { AsientoItem } from './asiento-item.entity';
 
 @Entity({ name: 'asiento' })
 export class Asiento {
-     @PrimaryGeneratedColumn()
-     id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-     // @Column({ nullable: true })
-     // fecha_emision: string;
-     @Column({ type: 'date', nullable: true })
-     fecha_emision: Date;
+  // @Column({ nullable: true })
+  // fecha_emision: string;
+  @Column({ type: 'date', nullable: true })
+  fecha_emision: Date;
 
-     @Column({ nullable: true })
-     nro_asiento: string;
+  @Column({ nullable: true })
+  nro_asiento: string;
 
-     @Column({ nullable: true })
-     comentario: string;
+  @Column({ nullable: true })
+  comentario: string;
 
-     @Column()
-     codigo_transaccion: string;
+  @Column()
+  codigo_transaccion: string;
 
-     @Column()
-     estado: string;
+  @Column()
+  estado: string;
 
-     @Column({ nullable: true })
-     nro_referencia: string;
+  @Column({ nullable: true })
+  nro_referencia: string;
 
-     @Column()
-     secuencial: string;
+  // @Column()
+  // secuencial: string;
 
-     @Column()
-     codigo_centro: string;
+  @Column()
+  codigo_centro: string;
 
-     @Column({ nullable: true })
-     codigo_empresa: string;
+  @Column({ nullable: true })
+  codigo_empresa: string;
 
-     @Column({ type: 'decimal', precision: 10, scale: 2 })
-     total_debe: number;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total_debe: number;
 
-     @Column({ type: 'decimal', precision: 10, scale: 2 })
-     total_haber: number;
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total_haber: number;
 
-     // Relación con AsientoItem
-     @OneToMany(() => AsientoItem, (asientoItem) => asientoItem.asiento)
-     lineItems: AsientoItem[];
+  // Relación con AsientoItem
+  @OneToMany(() => AsientoItem, (asientoItem) => asientoItem.asiento)
+  lineItems: AsientoItem[];
 }
