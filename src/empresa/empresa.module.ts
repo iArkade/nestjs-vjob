6ea4d25d12ae1c02ca690adbfad072fab6ef99d9
Daniel Velasco <desteban.velpi@gmail.com>
2025-01-16 19:35:01 -0500

    cambios en empresa para guardar el logo

--- src/empresa/empresa.module.ts
+++ src/empresa/empresa.module.ts
@@ -3,9 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Empresa } from './entities/empresa.entity';
 import { EmpresaController } from './empresa.controller';
 import { EmpresaService } from './empresa.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Empresa])],
+    imports: [
+        TypeOrmModule.forFeature([Empresa]),
+        ServeStaticModule.forRoot({
+            //rootPath: join(__dirname, '..', 'uploads'), // Directorio de archivos estáticos
+            rootPath: join(process.cwd(), 'uploads'),//uso esto porq el archivo esta fuera del src
+            serveRoot: '/uploads', // Ruta base para acceder a los archivos
+        }),
+    ],
     controllers: [EmpresaController],
     providers: [EmpresaService],
 })
