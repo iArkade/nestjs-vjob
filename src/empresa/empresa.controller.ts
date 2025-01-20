c7285108e2dcdfa7fecee0cca13d2fd1129bf371
David Proaño <davisprofranco@hotmail.es>
2025-01-20 15:14:19 -0500

    feat: fix conditions

--- src/empresa/empresa.controller.ts
+++ src/empresa/empresa.controller.ts
@@ -1,4 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query, Put, UseInterceptors, UploadedFile, BadRequestException, InternalServerErrorException, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  Put,
+  UseInterceptors,
+  UploadedFile,
+  BadRequestException,
+  InternalServerErrorException,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { CreateEmpresaDto } from './dto/create-empresa.dto';
 import { UpdateEmpresaDto } from './dto/update-empresa.dto';
 import { ApiTags } from '@nestjs/swagger';
@@ -11,68 +26,71 @@ import * as path from 'path';
 @ApiTags('empresa')
 @Controller('empresa')
 export class EmpresaController {
-    constructor(private readonly empresaService: EmpresaService) { }
-    @Get('all')
-    async findAll(): Promise<Empresa[]> {
-        return await this.empresaService.findAll();
-    }
-    //* Tengo que irme al Module para asegurarme de que sirva los archios desde el directorio de carga para que se puedan usar en el front end
-    @Post()
-    @UseInterceptors(
-        FileInterceptor('logo', {
-            storage: diskStorage({
-                destination: './uploads/logos', // Directorio para guardar los archivos
-                filename: (req, file, cb) => {
-                    try {
-                        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-                        const ext = file.originalname ? path.extname(file.originalname) : ''; // Manejar si `originalname` no existe
-                        console.log(file.originalname)
-                        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
-                    } catch (error) {
-                        cb(new Error('Error al procesar el archivo'), null);
-                    }
-                },
-            }),
-            fileFilter: (req, file, cb) => {
-                // Validar que sea una imagen
-                if (!file.mimetype.match(/image\/(jpeg|png|jpg)/)) {
-                    return cb(new Error('Solo se permiten archivos de imagen'), false);
-                }
-                if (!file.originalname) {
-                    return cb(new Error('El archivo no tiene un nombre válido'), false);
-                }
-                cb(null, true);
-            },
-            // limits: {
-            //     fileSize: 5 * 1024 * 1024, // 5 MB
-            // },
-        }),
-    )
-    async create(
-        @Body() createEmpresaDTO: CreateEmpresaDto,
-        @UploadedFile() file: Express.Multer.File
-    ){
-        if (file) {
-            // Generar URL completa del archivo
-            const baseUrl = process.env.BASE_URL
-            createEmpresaDTO.logo = `${baseUrl}/uploads/logos/${file.filename}`;
-            console.log( createEmpresaDTO.logo)
-            //createEmpresaDTO.logo = `/uploads/logos/${file.filename}`;
+  constructor(private readonly empresaService: EmpresaService) {}
+  @Get('all')
+  async findAll(): Promise<Empresa[]> {
+    return await this.empresaService.findAll();
+  }
+  //* Tengo que irme al Module para asegurarme de que sirva los archios desde el directorio de carga para que se puedan usar en el front end
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('logo', {
+      storage: diskStorage({
+        destination: './uploads/logos', // Directorio para guardar los archivos
+        filename: (req, file, cb) => {
+          try {
+            const uniqueSuffix =
+              Date.now() + '-' + Math.round(Math.random() * 1e9);
+            const ext = file.originalname
+              ? path.extname(file.originalname)
+              : ''; // Manejar si `originalname` no existe
+            console.log(file.originalname);
+            cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+          } catch (error) {
+            cb(new Error('Error al procesar el archivo'), null);
+          }
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        // Validar que sea una imagen
+        if (!file.mimetype.match(/image\/(jpeg|png|jpg|webp)/)) {
+          return cb(new Error('Solo se permiten archivos de imagen'), false);
         }
-
-        return await this.empresaService.create(createEmpresaDTO);
+        if (!file.originalname) {
+          return cb(new Error('El archivo no tiene un nombre válido'), false);
+        }
+        cb(null, true);
+      },
+      // limits: {
+      //     fileSize: 5 * 1024 * 1024, // 5 MB
+      // },
+    }),
+  )
+  async create(
+    @Body() createEmpresaDTO: CreateEmpresaDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (file) {
+      // Generar URL completa del archivo
+      const baseUrl = process.env.BASE_URL;
+      createEmpresaDTO.logo = `${baseUrl}/uploads/logos/${file.filename}`;
+      console.log(createEmpresaDTO.logo);
+      //createEmpresaDTO.logo = `/uploads/logos/${file.filename}`;
     }
 
-    @Put(':id')
-    async update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() updateEmpresaDTO: UpdateEmpresaDto,
-    ): Promise<Empresa> {
-        return await this.empresaService.update(id, updateEmpresaDTO);
-    }
+    return await this.empresaService.create(createEmpresaDTO);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
-        return await this.empresaService.delete(id);
-    }
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateEmpresaDTO: UpdateEmpresaDto,
+  ): Promise<Empresa> {
+    return await this.empresaService.update(id, updateEmpresaDTO);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+    return await this.empresaService.delete(id);
+  }
 }
