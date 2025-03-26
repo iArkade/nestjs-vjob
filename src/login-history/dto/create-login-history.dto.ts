485b434aac561b4237c60e742d519dabff0928d1
Daniel Velasco <desteban.velpi@gmail.com>
2025-03-26 14:50:00 -0500

    creacion de login-history para ver el inicio de sesion del usuario

---
+++ src/login-history/dto/create-login-history.dto.ts
@@ -0,0 +1,9 @@
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateLoginHistoryDto {
+     @IsNumber()
+     userId: number;
+
+     @IsString()
+     userName: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', true);
+
   //app.setGlobalPrefix('api')
   
   // Esto es importante al momento de usar class-Validator en los dtos
@@ -35,8 +38,9 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
   });
-
+  
 
   await app.listen(4000);
 }
+
 bootstrap();
